e54cf0367bf42a085f4ac3d3ea5bb2fc1956e0e4
Muhammadkaifsilawat <silawatkaif467@gmail.com>
2024-12-28 01:38:29 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let todolist = [];
+
+let a =prompt("What did you do all day today??")
+todolist.push(a)
+
+let b =prompt("What did you do all day today??")
+todolist.push(b)
+let c =prompt("What did you do all day today??")
+todolist.push(c)
+let d =prompt("What did you do all day today??")
+todolist.push(d)
+
+console.log("Your-todo-list");
+console.log(todolist);
